0903ba7d49fccd66f7b6a2536f36aab71588a790
GustavoMartinsPaz <gusttavopaz18@gmail.com>
2026-03-31 17:00:40 -0300

    operadores

---
+++ tlampada.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main(void){
+  for (int k = 7; k >= 0; k--) {
+    printf("%d ", k);
+  }
+  printf("\n");
+  return 0;
+}
